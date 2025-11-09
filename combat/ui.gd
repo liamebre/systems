cc28fd7fd4f54691f3b2d1a6192a8ab7edcd5283
liamebre <164887882+liamebre@users.noreply.github.com>
2025-11-09 14:49:26 -0500

    update
    added opps

--- combat/ui.gd
+++ combat/ui.gd
@@ -2,8 +2,17 @@ extends Control
 
 @export var fighters_node: Node
 @export var info_scene: PackedScene
+
 func initialize():
-	pass
+	for fighter in fighters_node.get_children():
+		var health = fighter.get_node("health")
+		var info = info_scene.instantiate()
+		var health_info = info.get_node("VBoxContainer/VBoxContainer/ProgressBar")
+		health_info.value = health.life
+		health_info.max_value = health.max_life
+		info.get_node("VBoxContainer/CenterContainer/Label").text = fighter.name
+		health.health_changed.connect(health_info.set_value)
+		$fighters.add_child(info)
 
 func _on_attack_button_down() -> void:
 	pass # Replace with function body.
