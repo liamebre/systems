fa53fe4d081e6cdbe8835fd8e32eddc06a2fd7cd
liamebre <164887882+liamebre@users.noreply.github.com>
2025-11-10 14:36:17 -0500

    update

--- main.gd
+++ main.gd
@@ -5,7 +5,8 @@ extends Node2D
 @export var exploration_screen: Node
 
 
-func start_combat():
+func start_combat(combat_actors):
 	remove_child($map)
 	add_child($fight)
+	combat_screen.initialize(combat_actors)
 	combat_screen.show()
